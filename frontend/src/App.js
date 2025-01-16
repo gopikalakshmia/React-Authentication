2dc76153e4f3c44c81e81d483a51c3f4179cb486
gopika <gopikalakshmia@gmail.com>
2025-01-16 14:41:41 -0500

    added authentication to manually directed route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import Authentication from './pages/Authentication';
 import {authenticationAction} from './pages/Authentication';
-import { getToken, tokenLoader } from './util/auth';
+import { checkAuthLoader, getToken, tokenLoader } from './util/auth';
 import Logout, { logoutAction } from './pages/Logout';
 
 const router = createBrowserRouter([
@@ -50,6 +50,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader:checkAuthLoader
               },
             ],
           },
@@ -57,6 +58,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader:checkAuthLoader
           },
         ],
       },
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -9,3 +9,11 @@ return localStorage.getItem('token');
 export const tokenLoader=()=>{
     return localStorage.getItem('token');
 }
+
+export const checkAuthLoader=()=>{
+    const token=localStorage.getItem('token');
+    if(!token)
+        return redirect("/auth");
+    return null
+
+}
